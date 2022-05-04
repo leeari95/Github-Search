3047fc182c10bd7d3db662ccd9099ad59b352acb
Ari <lee_ari95@icloud.com>
2022-05-05 04:03:58 +0900

    ✨ [feat] 로그인 기능을 담당하는 LoginManager 타입 구현
    - 로그인 성공시 Token을 저장하는 역할을 하는 KeychainManager 타입 생성
    - SceneDelegate에 Token을 요청하는 기능 추가
    - Info.plist에 OAuth로 로그인 후 다시 앱으로 돌아올 수 있는 URLSchemes 추가
    - API 요청시 필요한 URL을 모아둔 APIAdress 타입 생성
    - .gitignore에 Secrets.swift 파일 추가

---
+++ GithubSearchApp/GithubSearchApp/LoginManager.swift
@@ -0,0 +1,55 @@
+//
+//  LoginManager.swift
+//  GithubSearchApp
+//
+//  Created by Ari on 2022/05/04.
+//
+
+import UIKit
+
+
+struct LoginManager {
+    private let apiRequest: DefaultAPIProvider
+    static var isLogin: Bool = KeychainManager.shard.load("Token") == nil ? false : true
+    
+    init(apiRequest: DefaultAPIProvider = DefaultAPIProvider()) {
+        self.apiRequest = apiRequest
+    }
+    
+    func authorize() {
+        guard let url = APIAddress.code(clientID: Secrets.clinetID, scope: "repo,user").url else {
+            return
+        }
+        UIApplication.shared.open(url)
+    }
+    
+    func requestToken(code: String) {
+        guard let url = APIAddress.token(clientID: Secrets.clinetID, clientSecret: Secrets.clinetSecret, code: code).url else {
+            return
+        }
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        apiRequest.execute(request: request) { result in
+            switch result {
+            case .success(let data):
+                data.flatMap { try? JSONSerialization.jsonObject(with: $0, options: []) as? [String: String] }
+                .flatMap { $0["access_token"] }
+                .flatMap {
+                    print($0)
+                    if KeychainManager.shard.save(key: "Token", value: $0) {
+                        print("사용자의 토큰을 키체인에 저장하는데 성공했습니다!")
+                    } else {
+                        print("토큰을 가져오지 못했습니다.")
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func logout() {
+        KeychainManager.shard.delete(key: "Token")
+    }
+}
