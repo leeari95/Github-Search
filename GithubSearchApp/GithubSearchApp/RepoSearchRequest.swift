c1d45898bebd3b15e6ecbd2dc9bfbe0638f4ef45
Ari <lee_ari95@icloud.com>
2022-05-05 17:59:23 +0900

    ✨ [feat] 레파지토리를 검색할 때 필요한 Request와 Response 타입을 생성
    - APIRequest를 채택하고 있는 RepoSearchRequest 타입을 구현
    - API 요청시 내려오는 Response 타입 RepoSearchResponseDTO을 구현
    - 뷰에서 활용할 모델 RepositoryItem 타입을 구현

---
+++ GithubSearchApp/GithubSearchApp/RepoSearchRequest.swift
@@ -0,0 +1,44 @@
+//
+//  RepoSearchRequest.swift
+//  GithubSearchApp
+//
+//  Created by Ari on 2022/05/05.
+//
+
+import Foundation
+
+struct RepoSearchRequest: APIRequest {
+    typealias Response = RepoSearchResponseDTO
+    let method: HTTPMethod = .get
+    let baseURL: URL? = URL(string: "https://api.github.com/")
+    let path: String = "search/repositories"
+    let headers: [String : String]? = ["Accept": "application/vnd.github.v3+json"]
+    private var keyword: String
+    private var perPage: UInt
+    let page: UInt
+    
+    var parameters: [String : String] {
+        [
+            "q": keyword,
+            "per_page": perPage.description,
+            "page": page.description
+        ]
+    }
+    
+    init(keyword: String, page: UInt = 1, perPage: UInt = 20) {
+        self.keyword = keyword
+        self.page = page
+        self.perPage = perPage
+    }
+    
+    mutating func editSearchWord(to searchWord: String) {
+        keyword = searchWord
+    }
+    
+    mutating func nextPage() {
+        perPage += 20
+    }
+}
+
+
+
