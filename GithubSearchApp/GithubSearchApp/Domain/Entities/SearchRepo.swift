d4d1ee23c1976985af662591486d28383ecc9e4f
Ari <lee_ari95@icloud.com>
2022-05-07 02:48:15 +0900

    ✨ [feat] SearchRepo 엔티티 구현 및 기존 Response 리팩토링
    - 도메인으로 변환해주는 메소드를 하단으로 위치 이동
    - 불필요한 import 제거 및 주석 추가

---
+++ GithubSearchApp/GithubSearchApp/Domain/Entities/SearchRepo.swift
@@ -0,0 +1,13 @@
+//
+//  SearchRepository.swift
+//  GithubSearchApp
+//
+//  Created by Ari on 2022/05/07.
+//
+
+import Foundation
+
+struct SearchRepo {
+    let totalCount: Int
+    let items: [RepositoryItem]
+}
