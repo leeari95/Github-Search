d9f37e99b4f6eccb4d64bc31be02a3f41e424870
Ari <lee_ari95@icloud.com>
2022-05-07 02:02:31 +0900

    ♻️  [refactor] APIProvider, DefaultAPIProvider 리팩토링
    - APIProvider 내부에 excute 메소드 정의 추가
    - DefaultAPIProvider의 excute 메소드 내부에 상태코드 뿐만 아니라 메세지도 출력할 수 있도록 개선

--- GithubSearchApp/GithubSearchApp/Data/Netwoking/Protocol/APIProvider.swift
+++ GithubSearchApp/GithubSearchApp/Data/Netwoking/Protocol/APIProvider.swift
@@ -9,10 +9,14 @@ import Foundation
 
 protocol APIProvider {
     var session: URLSession { get }
+    
+    func execute(request: URLRequest, completion: @escaping (Result<Data?, Error>) -> Void)
+    
     func request<T: APIRequest>(
         _ request: T,
         completion: @escaping (Result<T.Response, Error>) -> Void
     )
+    
     func requestList<T: APIRequest>(
         _ request: T,
         completion: @escaping (Result<[T.Response], Error>) -> Void
