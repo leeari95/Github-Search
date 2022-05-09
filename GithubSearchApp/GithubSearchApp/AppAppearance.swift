addbdc80549a4b48bdd4751c1e5f094f3eb0647e
Ari <lee_ari95@icloud.com>
2022-05-09 10:27:31 +0900

    ✨ [feat]  Main 스토리보드에 UI 요소를 추가 및 UI 초기설정
    - 탭바와 네비게이션 컨트롤러 추가
    - 네비게이션 타이틀을 Large로 변경
    - Info.plist에 Appearance를 추가하여 다크모드로 작동하도록 구성
    - AppAppearance 타입을 추가 및 AppDelegate에 앱의 appearance를 설정하도록 구성

---
+++ GithubSearchApp/GithubSearchApp/AppAppearance.swift
@@ -0,0 +1,15 @@
+//
+//  AppAppearance.swift
+//  GithubSearchApp
+//
+//  Created by Ari on 2022/05/08.
+//
+
+import UIKit
+
+final class AppAppearance {
+    static func setUpAppearance() {
+        UITabBar.appearance().tintColor = .label
+        UIBarButtonItem.appearance().tintColor = .label
+    }
+}
