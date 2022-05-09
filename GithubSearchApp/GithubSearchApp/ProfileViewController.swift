a9e0ca96aa9f0c4d2cd0b459018ba271ee5e75cc
Ari <lee_ari95@icloud.com>
2022-05-09 14:10:58 +0900

    ✨ [feat] 두번째 탭바 컨트롤러의 뷰컨인 ProfileViewController 추가

---
+++ GithubSearchApp/GithubSearchApp/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  GithubSearchApp
+//
+//  Created by Ari on 2022/05/08.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Profile"
+        
+        let loginButton = UIBarButtonItem(title: "Login", style: .plain, target: nil, action: nil)
+        loginButton.tintColor = .label
+        navigationItem.rightBarButtonItem = loginButton
+        
+    }
+}
