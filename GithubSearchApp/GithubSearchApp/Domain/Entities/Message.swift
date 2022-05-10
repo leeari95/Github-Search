63847799a612b3e282b2d54f7943a9982c0ba87d
Ari <lee_ari95@icloud.com>
2022-05-10 22:16:30 +0900

    ✨ [feat] 별표 버튼을 클릭했을 때 UserUseCase에게 알림을 보내도록 구현
    - UserUseCase 이니셜라이저에 NotificationCenter.addObserver 메소드 호출
    - 기존 setUp 메소드 삭제하고, Notification Selector 메소드를 구현
    - Notification.Name 확장 추가

---
+++ GithubSearchApp/GithubSearchApp/Domain/Entities/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  GithubSearchApp
+//
+//  Created by Ari on 2022/05/10.
+//
+
+import Foundation
+
+struct Message {
+    let title: String
+    let description: String
+}
