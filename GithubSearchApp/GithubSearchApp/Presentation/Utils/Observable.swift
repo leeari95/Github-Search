7e8c58701134abc5bf718c1e3a38b3ff26a89b75
Ari <lee_ari95@icloud.com>
2022-05-10 20:12:42 +0900

    ✨ [feat] Observable 타입 구현

---
+++ GithubSearchApp/GithubSearchApp/Presentation/Utils/Observable.swift
@@ -0,0 +1,34 @@
+//
+//  Observable.swift
+//  GithubSearchApp
+//
+//  Created by Ari on 2022/05/09.
+//
+
+import Foundation
+
+class Observable<T> {
+    typealias Observer = (T) -> Void
+    var observer: Observer?
+    
+    func observe(_ observer: Observer?) {
+        self.observer = observer
+        guard let value = value else {
+            return
+        }
+        observer?(value)
+    }
+    
+    var value: T? {
+        didSet {
+            guard let value = value else {
+                return
+            }
+            observer?(value)
+        }
+    }
+    
+    init(_ value: T?) {
+        self.value = value
+    }
+}
